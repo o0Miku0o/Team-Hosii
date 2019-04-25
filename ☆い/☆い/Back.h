7d7dadf5d36af681f2522cbfbe7a2fa4c18cfe16
Kitauji <1>
2019-04-26 00:54:50 +0900

    メソッド追加（動作は同じ）
    コードの直書きをメソッド化していきます。

--- ☆い/☆い/Back.h
+++ ☆い/☆い/Back.h
@@ -21,18 +21,26 @@ namespace Back
 	{
 	public:
 		/*ïKóvÇ»ÉÅÉìÉoÇÕÇ±Ç±Ç…í«â¡*/
+
+		Obj() {}
+		~Obj() {}
+	private:
 		Pixel pBackStar[560];
 		Rec rBackBase;
 		Line lShootingStar;
 		byte bSSLife;
 		byte bSSLifeMax;
 
-		Obj() {}
-		~Obj() {}
-	private:
 		RS_ptr res;
 		void Init();
 		void Finalize();
+
+		void ShootingStarUpdate();
+		void BackStarUpdate();
+
+		void BackBaseDraw();
+		void ShootingStarDraw();
+		void BackStarDraw();
 	public:
 		void Update();
 		void Render();
