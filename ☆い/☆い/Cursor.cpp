031714db1de05ff47eff4a8d58146a72382abc15
AineSylphia <dkdifd@hotmail.com>
2019-08-30 18:41:42 +0900

    マージ作業中に発見したバグとかの修正

--- ☆い/☆い/Cursor.cpp
+++ ☆い/☆い/Cursor.cpp
@@ -101,111 +101,10 @@ namespace Cursor
 		}
 		bool bHitFlag = false;
 		constexpr float fAddScale = 70.f;
-//<<<<<<< HEAD
-		//constexpr float fScaleWMax = 1800.f;
-		//constexpr float fScaleHMax = 400.f;
-		//bool bFlag = false;
-		//Hukidasi::StageGroup sGroup = Hukidasi::StageGroup::GROUP_EARTH;
-		//if (auto us = Find<StageSelectObjEarth::Obj>(StageSelectObjEarth::caTaskName))
-		//{
-		//	us->rEarth.Scaling(16 * 10, 16 * 10);
-		//	Circle cHit(&us->rEarth.GetPos(), us->rEarth.GetW() * 0.5f);
-		//	if (cHit.CheckHit(&rCursorBase.GetPos()))
-		//	{
-		//		us->rEarth.Scaling(16 * 15, 16 * 15);
-		//		bFlag = true;
-
-		//		sGroup = Hukidasi::StageGroup::GROUP_EARTH;
-
-		//		//ééóVâÔ
-		//		if (kb->Down(VK_RETURN) || kb->Down(VK_RIGHT) || pad->Down(JOY_BUTTON6) || pad->Down(JOY_BUTTON2)) {
-		//			RemoveAll(StageManager::caTaskName, NOT_REMOVE_NAME);
-		//			if (auto manager = Find<StageManager::Obj>(StageManager::caTaskName)) {
-		//				manager->bStageNum = 11;
-		//				if (manager->bStageNum == 255) {
-		//					RemoveAll();
-		//					Add<StageManager::Obj>();
-		//					Add<Back::Obj>();
-		//					Add<StageSelect::Obj>();
-		//					Pause(2);
-		//				}
-		//				else {
-		//					Add<StageLoad::Obj>();
-		//					Pause(2);
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
-		//if (auto us = Find<StageSelectObjAsteroid::Obj>(StageSelectObjAsteroid::caTaskName))
-		//{
-		//	us->rAsteroid.Scaling(16 * 10, 16 * 10);
-		//	Circle cHit(&us->rAsteroid.GetPos(), us->rAsteroid.GetW() * 0.5f);
-		//	if (cHit.CheckHit(&rCursorBase.GetPos()))
-		//	{
-		//		us->rAsteroid.Scaling(16 * 15, 16 * 15);
-		//		bFlag = true;
-
-		//		sGroup = Hukidasi::StageGroup::GROUP_ASTEROID;
-
-		//		//ééóVâÔ
-		//		if (kb->Down(VK_RETURN) || kb->Down(VK_RIGHT) || pad->Down(JOY_BUTTON6) || pad->Down(JOY_BUTTON2)) {
-		//			RemoveAll(StageManager::caTaskName, NOT_REMOVE_NAME);
-		//			if (auto manager = Find<StageManager::Obj>(StageManager::caTaskName)) {
-		//				manager->bStageNum = 21;
-		//				if (manager->bStageNum == 255) {
-		//					RemoveAll();
-		//					Add<StageManager::Obj>();
-		//					Add<Back::Obj>();
-		//					Add<StageSelect::Obj>();
-		//					Pause(2);
-		//				}
-		//				else {
-		//					Add<StageLoad::Obj>();
-		//					Pause(2);
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
-		//if (auto us = Find<StageSelectObjGalaxy::Obj>(StageSelectObjGalaxy::caTaskName))
-		//{
-		//	us->rGalaxy.Scaling(16 * 10, 16 * 10);
-		//	Circle cHit(&us->rGalaxy.GetPos(), us->rGalaxy.GetW() * 0.5f);
-		//	if (cHit.CheckHit(&rCursorBase.GetPos()))
-		//	{
-		//		us->rGalaxy.Scaling(16 * 15, 16 * 15);
-		//		bFlag = true;
-
-		//		sGroup = Hukidasi::StageGroup::GROUP_GALAXY;
-
-		//		//ééóVâÔ
-		//		if (kb->Down(VK_RETURN) || kb->Down(VK_RIGHT) || pad->Down(JOY_BUTTON6) || pad->Down(JOY_BUTTON2)) {
-		//			RemoveAll(StageManager::caTaskName, NOT_REMOVE_NAME);
-		//			if (auto manager = Find<StageManager::Obj>(StageManager::caTaskName)) {
-		//				manager->bStageNum = 31;
-		//				if (manager->bStageNum == 255) {
-		//					RemoveAll();
-		//					Add<StageManager::Obj>();
-		//					Add<Back::Obj>();
-		//					Add<StageSelect::Obj>();
-		//					Pause(2);
-		//				}
-		//				else {
-		//					Add<StageLoad::Obj>();
-		//					Pause(2);
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
-		//if (auto us = Find<StageSelectObjUS::Obj>(StageSelectObjUS::caTaskName))
-//=======
 		constexpr float fScaleWMax = 1800.f;//2000;
 		constexpr float fScaleHMax = 400.f;//600;
 		auto hu = Find<Hukidasi::Obj>(Hukidasi::caTaskName);
 		for (auto si : FindAll<StageSelectIcon::Obj>(StageSelectIcon::caTaskName))
-//>>>>>>> origin/Develop
 		{
 			si->rIcon.Scaling(16.f * 10.f, 16.f * 10.f);
 			Circle cHit;
@@ -213,17 +112,8 @@ namespace Cursor
 			cHit.SetRadius(si->rIcon.GetW() / 2);
 			if (cHit.CheckHit(&rCursorBase.GetPos()))
 			{
-//<<<<<<< HEAD
-//				us->rUranus.Scaling(16 * 15, 16 * 15);
-//				bFlag = true;
-//
-//				sGroup = Hukidasi::StageGroup::GROUP_URANUS;
-//
-//				if (kb->Down(VK_RETURN) || kb->Down(VK_RIGHT) || pad->Down(JOY_BUTTON6) || pad->Down(JOY_BUTTON2))
-//=======
 				si->rIcon.Scaling(16.f * 15.f, 16.f * 15.f);
 				if (kb->Down(VK_RETURN) || pad->Down(JOY_BUTTON6))
-//>>>>>>> origin/Develop
 				{
 					RemoveAll(StageManager::caTaskName, NOT_REMOVE_NAME);
 					if (auto manager = Find<StageManager::Obj>(StageManager::caTaskName))
@@ -235,14 +125,7 @@ namespace Cursor
 						Pause(2);
 					}
 				}
-//
-//<<<<<<< HEAD
-//				sGroup = Hukidasi::StageGroup::GROUP_BLACKHOLE;
-//
-//				if (kb->Down(VK_RETURN) || kb->Down(VK_RIGHT) || pad->Down(JOY_BUTTON6) || pad->Down(JOY_BUTTON2))
-//=======
 				if (hu)
-//>>>>>>> origin/Develop
 				{
 					Point pPos(Rec::Win.r * 0.5f, Rec::Win.b * 0.75f);
 					hu->SetPos(&pPos);
@@ -257,25 +140,6 @@ namespace Cursor
 				if (hu)hu->SetAddScale(-fAddScale);
 			}
 		}
-
-		//if (auto hu = Find<Hukidasi::Obj>(Hukidasi::caTaskName))
-		//{
-		//	constexpr float fAddScale = 70.f;
-		//	constexpr float fScaleWMax = 1800.f;//2000;
-		//	constexpr float fScaleHMax = 400.f;//600;
-		//	if (bHitFlag)
-		//	{
-		//		Point pPos(Rec::Win.r * 0.5f, Rec::Win.b * 0.75f);
-		//		hu->SetPos(&pPos);
-		//		hu->SetScaleMax(fScaleWMax, fScaleHMax);
-		//		hu->SetAddScale(fAddScale);
-		//		hu->SetStageGroup(sGroup);
-		//	}
-		//	else
-		//	{
-		//		hu->SetAddScale(-fAddScale);
-		//	}
-		//}
 	}
 	/*É^ÉXÉNÇÃï`âÊèàóù*/
 	void Obj::Render()
@@ -308,38 +172,30 @@ namespace Cursor
 		}
 		if (apKB->On('D'))
 		{
-//<<<<<<< HEAD
-//			rCursorBase.Move(&(Vector2::right * fSpd));
-//		}
-//		float fX = rCursorBase.GetPosX();
-//		float fY = rCursorBase.GetPosY();
-//		const float fW = rCursorBase.GetW();
-//		const float fH = rCursorBase.GetH();
-//		if (fX <= Rec::Win.l + fW * 0.5f)
-//		{
-//			fX = Rec::Win.l + fW * 0.5f;
-//		}
-//		else if (fX >= Rec::Win.r - fW * 0.5f)
-//		{
-//			fX = Rec::Win.r - fW * 0.5f;
-//		}
-//		if (fY <= Rec::Win.t + fH * 0.5f)
-//		{
-//			fY = Rec::Win.t + fH * 0.5f;
-//		}
-//		else if (fY >= Rec::Win.b - fH * 0.5f)
-//		{
-//			fY = Rec::Win.b - fH * 0.5f;
-//		}
-//		rCursorBase.SetPos(&Point(fX, fY));
-//=======
-			if (afX >= Rec::Win.r - afW / 2) {
-				rCursorBase.SetPos(&Point(Rec::Win.r - afW / 2, afY));
-			}
-			else
-				rCursorBase.Move(&(Vector2::right * fSpd));
+			rCursorBase.Move(&(Vector2::right * fSpd));
 		}
-//>>>>>>> origin/Develop
+		//âÊñ äOÇÃîªíËèàóù
+		float fX = rCursorBase.GetPosX();
+		float fY = rCursorBase.GetPosY();
+		const float fW = rCursorBase.GetW();
+		const float fH = rCursorBase.GetH();
+		if (fX <= Rec::Win.l + fW * 0.5f)
+		{
+			fX = Rec::Win.l + fW * 0.5f;
+		}
+		else if (fX >= Rec::Win.r - fW * 0.5f)
+		{
+			fX = Rec::Win.r - fW * 0.5f;
+		}
+		if (fY <= Rec::Win.t + fH * 0.5f)
+		{
+			fY = Rec::Win.t + fH * 0.5f;
+		}
+		else if (fY >= Rec::Win.b - fH * 0.5f)
+		{
+			fY = Rec::Win.b - fH * 0.5f;
+		}
+		rCursorBase.SetPos(&Point(fX, fY));
 	}
 	/*ÉpÉbÉhÇ≈ÇÃà⁄ìÆ*/
 	void Obj::MovePad(std::shared_ptr<JoyPad> &apPad)
